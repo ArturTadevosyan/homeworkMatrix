f2d68e13baa7ab7795b8b902cca9384716d4e826
ArturTadevosyan <arturtadevosyan926@gmail.com>
2023-07-08 16:13:12 +0400

    first commit

---
+++ src/masiv211_230/Matrix.java
@@ -0,0 +1,7 @@
+package masiv211_230;
+
+public class Matrix {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/masiv251_260/Matrix1.java
@@ -0,0 +1,7 @@
+package masiv251_260;
+
+public class Matrix1 {
+    public static void main(String[] args) {
+
+    }
+}
