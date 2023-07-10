b3ccba1fe5fa5ccadaffb3c1564761174efd0239
ArturTadevosyan <arturtadevosyan926@gmail.com>
2023-07-10 20:43:00 +0400

    rename package names

--- src/array251_260/Task260.java
+++ src/array251_260/Task260.java
@@ -3,12 +3,12 @@ package array251_260;
 public class Task260 {
     public static void main(String[] args) {
 
-        int[] array = {10, 7, -6, -7, 12, 120, 44, -10, 15, 66, -33, 54, -20};
+        int[] array = {-33, 7, -6, -7, 12, 120, 44, -10, 15, 66, -33, 54, -20};
 
         int min = array[0];
         int index = 0;
         for (int i = 1; i < array.length; i++) {
-            if (array[i] < min) {
+            if (array[i] <= min) {
                 min = array[i];
                 index = i;
             }
